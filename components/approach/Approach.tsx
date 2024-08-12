3e5de729fb8eca87742c8befa649c9f1c2901b3d
ALDRICH_CODE45 <nocheblanca92@gmail.com>
2024-08-12 17:59:10 -0600

    feat:my_approach_translated

--- components/approach/Approach.tsx
+++ components/approach/Approach.tsx
@@ -8,17 +8,18 @@ const Approach = () => {
   return (
     <section className="w-full py-20">
       <h1 className="heading">
-        My <span className="text-purple">approach</span>
+        Mi <span className="text-purple">enfoque</span>
       </h1>
       {/* remove bg-white dark:bg-black */}
       <div className="my-20 flex flex-col lg:flex-row items-center justify-center w-full gap-4">
         {/* add des prop */}
         <Card
-          title="Planning & Strategy"
+          title="Planificación & Estrategia"
           icon={<AceternityIcon order="Phase 1" />}
-          des="We'll collaborate to map out your website's goals, target audience, 
-          and key functionalities. We'll discuss things like site structure, 
-          navigation, and content requirements."
+          des="Colaboraremos para definir los objetivos de tu sitio web, 
+          el público objetivo y las funcionalidades clave. Discutiremos 
+          aspectos como la estructura del sitio, la navegación y los 
+          requisitos de contenido."
         >
           <CanvasRevealEffect
             animationSpeed={5.1}
@@ -27,11 +28,10 @@ const Approach = () => {
           />
         </Card>
         <Card
-          title="Development & Progress Update"
+          title="Desarrollo & Actualización de Progreso"
           icon={<AceternityIcon order="Phase 2" />}
-          des="Once we agree on the plan, I cue my lofi playlist and dive into
-          coding. From initial sketches to polished code, I keep you updated
-          every step of the way."
+          des="Una vez que estemos de acuerdo con el plan, pongo mi lista de reproducción de lofi y empiezo a programar. 
+          Desde los bocetos iniciales hasta el código final, te mantendré informado en cada paso del proceso."
         >
           <CanvasRevealEffect
             animationSpeed={3}
@@ -49,11 +49,10 @@ const Approach = () => {
           {/* <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" /> */}
         </Card>
         <Card
-          title="Development & Launch"
+          title="Desarrollo & Lanzamiento"
           icon={<AceternityIcon order="Phase 3" />}
-          des="This is where the magic happens! Based on the approved design, 
-          I'll translate everything into functional code, building your website
-          from the ground up."
+          des="¡Aquí es donde sucede la magia! Con base en el diseño aprobado, 
+          traduciré todo a código funcional, construyendo tu sitio web desde cero."
         >
           <CanvasRevealEffect
             animationSpeed={3}
@@ -165,24 +164,6 @@ const AceternityIcon = ({ order }: { order: string }) => {
         </span>
       </button>
     </div>
-    // remove the svg and add the button
-    // <svg
-    //   width="66"
-    //   height="65"
-    //   viewBox="0 0 66 65"
-    //   fill="none"
-    //   xmlns="http://www.w3.org/2000/svg"
-    //   className="h-10 w-10 text-black dark:text-white group-hover/canvas-card:text-white "
-    // >
-    //   <path
-    //     d="M8 8.05571C8 8.05571 54.9009 18.1782 57.8687 30.062C60.8365 41.9458 9.05432 57.4696 9.05432 57.4696"
-    //     stroke="currentColor"
-    //     strokeWidth="15"
-    //     strokeMiterlimit="3.86874"
-    //     strokeLinecap="round"
-    //     style={{ mixBlendMode: "darken" }}
-    //   />
-    // </svg>
   );
 };
 
